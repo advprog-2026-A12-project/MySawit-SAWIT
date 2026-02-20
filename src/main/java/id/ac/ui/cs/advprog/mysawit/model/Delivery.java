38dffbf7c0d691a4503e4c4be5a1a1bd3c3f3874
Ahmad Nizar Sauki <anizarsauki@gmail.com>
2026-02-20 15:12:15 +0700

    feat: init delivery model

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/model/Delivery.java
@@ -0,0 +1,42 @@
+package id.ac.ui.cs.advprog.mysawit.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "harvest_deliveries")
+@Getter
+@Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Delivery {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.UUID)
+   private String id;
+
+   @Column(nullable = false)
+   private String supirName;
+
+   @Column(nullable = false)
+   @Min(value = 1, message = "Muatan tidak boleh kosong")
+   @Max(value = 400, message = "Muatan tidak boleh melebihi 400 Kg")
+   private Double payloadKg;
+
+   @Column(nullable = false)
+   private String status = "MEMUAT";
+
+   @Column(name = "created_at")
+   private LocalDateTime createdAt;
+
+   @PrePersist
+   protected void onCreate() {
+      this.createdAt = LocalDateTime.now();
+   }
+}
