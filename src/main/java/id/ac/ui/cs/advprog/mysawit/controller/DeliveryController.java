4028c6fd1990ed0313e0fdb92b4642ef44512de7
Ahmad Nizar Sauki <anizarsauki@gmail.com>
2026-02-20 15:48:45 +0700

    fix: import format sesuai checkstyle

--- src/main/java/id/ac/ui/cs/advprog/mysawit/controller/DeliveryController.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/controller/DeliveryController.java
@@ -4,7 +4,11 @@ import id.ac.ui.cs.advprog.mysawit.model.Delivery;
 import id.ac.ui.cs.advprog.mysawit.repository.DeliveryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
--- src/main/java/id/ac/ui/cs/advprog/mysawit/model/Delivery.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/model/Delivery.java
@@ -1,10 +1,16 @@
 package id.ac.ui.cs.advprog.mysawit.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.PrePersist;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
