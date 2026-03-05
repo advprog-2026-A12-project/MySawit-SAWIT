1db0a67a714b827a7faee7ae5bb4e1ab5cbf2c46
Ahmad Nizar Sauki <anizarsauki@gmail.com>
2026-03-05 14:10:39 +0700

    refactor: edit model

--- src/main/java/id/ac/ui/cs/advprog/mysawit/delivery/model/Delivery.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/delivery/model/Delivery.java
@@ -6,15 +6,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
-import jakarta.persistence.PrePersist;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 @Entity
 @Table(name = "harvest_deliveries")
@@ -25,24 +27,37 @@ public class Delivery {
 
    @Id
    @GeneratedValue(strategy = GenerationType.UUID)
+   @Column(columnDefinition = "uuid")
    private String id;
 
-   @Column(nullable = false)
+   @NotNull
+   @Column(name = "supir_id", columnDefinition = "uuid")
+   private String supirId;
+
+   @Column(name = "supir_name")
    private String supirName;
 
-   @Column(nullable = false)
+   @NotNull
+   @Column(name = "mandor_id", columnDefinition = "uuid")
+   private String mandorId;
+
+   @NotNull
+   @Column(name = "harvest_id", columnDefinition = "uuid")
+   private String harvestId;
+
+   @Column(name = "payload_kg", nullable = false)
    @Min(value = 1, message = "Muatan tidak boleh kosong")
    @Max(value = 400, message = "Muatan tidak boleh melebihi 400 Kg")
    private Double payloadKg;
 
-   @Column(nullable = false)
+   @Column(nullable = false, length = 50)
    private String status = "MEMUAT";
 
-   @Column(name = "created_at")
-   private LocalDateTime createdAt;
+   @CreationTimestamp
+   @Column(name = "created_at", updatable = false)
+   private ZonedDateTime createdAt;
 
-   @PrePersist
-   protected void onCreate() {
-      this.createdAt = LocalDateTime.now();
-   }
+   @UpdateTimestamp
+   @Column(name = "updated_at")
+   private ZonedDateTime updatedAt;
 }
