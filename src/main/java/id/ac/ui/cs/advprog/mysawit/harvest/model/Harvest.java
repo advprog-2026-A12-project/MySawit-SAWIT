fec8553da17fafeda5bec18a7c5c9a58506707f6
unknown <hakimandi976@gmail.com>
2026-04-15 21:51:26 +0700

    fix checkstyle

--- src/main/java/id/ac/ui/cs/advprog/mysawit/harvest/model/Harvest.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/harvest/model/Harvest.java
@@ -1,7 +1,20 @@
 package id.ac.ui.cs.advprog.mysawit.harvest.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
