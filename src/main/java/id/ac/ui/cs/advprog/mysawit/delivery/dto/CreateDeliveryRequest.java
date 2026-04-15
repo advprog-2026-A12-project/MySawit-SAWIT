16b25b442c266706ee826ff6dd0be292f5c5b14d
Ahmad Nizar Sauki <anizarsauki@gmail.com>
2026-04-15 17:46:53 +0700

    feat(delivery): add payload validation constraints to CreateDeliveryRequest

--- src/main/java/id/ac/ui/cs/advprog/mysawit/delivery/dto/CreateDeliveryRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/delivery/dto/CreateDeliveryRequest.java
@@ -1,13 +1,24 @@
 package id.ac.ui.cs.advprog.mysawit.delivery.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
 public class CreateDeliveryRequest {
+    @NotNull
     private UUID supirId;
+
+    @NotNull
     private UUID harvestId;
+
+    @NotNull
+    @Min(value = 1, message = "Payload minimal 1 Kg")
+    @Max(value = 400, message = "Payload maksimal 400 Kg")
     private Double payloadKg;
+
     private String supirName;
 }
